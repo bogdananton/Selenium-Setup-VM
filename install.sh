7cf96e95044668c23cec3c135deac2536851f247
Bogdan Anton <contact@bogdananton.ro>
2015-11-21 03:35:59 +0200

    Added install.sh
    #5

---
+++ install.sh
@@ -0,0 +1,25 @@
+
+export vagrantInstalled=`vagrant -v`
+export virtualboxInstalled=`virtualbox -v`
+
+if ! $vagrantInstalled && $virtualboxInstalled ; then
+    # failed, some components are missing
+
+    distroId=`cat /etc/*-release | grep "ID="`
+    echo "Found distribution ID: ${distroId:3}" # @todo check if the pattern is ok
+
+    if [ "${distroId:3}" == "antergos" ]; then
+        # will (re)install packages after syncronization and not ask for confirmation
+        sudo pacman -S --noconfirm vagrant virtualbox
+
+    elif [ "${distroId:3}" == "Ubuntu" ]; then
+        # fixme untested yet (lowercase/uppercase)
+        sudo apt-get -y install vagrant virtualbox
+    else
+        echo "Error: Install vagrant and virtualbox manually or add a command in this install.sh file."
+        exit
+    fi
+fi
+
+vagrant plugin install vagrant-proxyconf
+vagrant up
